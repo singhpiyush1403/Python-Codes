cc91a56496d936181c5ecce1699570487d7870f8
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-01-10 10:02:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+
+
+integer_type = 10
+float_type = 10.5
+complex_type = 3 + 4j
+
+string_type = "Hello, World!"
+
+
+list_type = [1, 2, 3, 4, 5]
+tuple_type = (1, 2, 3, 4, 5)
+range_type = range(1, 6)
+
+
+set_type = {1, 2, 3, 4, 5}
+frozenset_type = frozenset({1, 2, 3, 4, 5})
+
+
+dict_type = {"key1": "value1", "key2": "value2"}
+
+
+boolean_type = True
+
+
+bytes_type = b"Hello, World!"
+bytearray_type = bytearray(5)
+memoryview_type = memoryview(bytes_type)
+
+
+none_type = None
+
+
+print("Integer type:", type(integer_type))
+print("Float type:", type(float_type))
+print("Complex type:", type(complex_type))
+print("String type:", type(string_type))
+print("List type:", type(list_type))
+print("Tuple type:", type(tuple_type))
+print("Range type:", type(range_type))
+print("Set type:", type(set_type))
+print("Frozenset type:", type(frozenset_type))
+print("Dictionary type:", type(dict_type))
+print("Boolean type:", type(boolean_type))
+print("Bytes type:", type(bytes_type))
+print("Bytearray type:", type(bytearray_type))
+print("Memoryview type:", type(memoryview_type))
+print("None type:", type(none_type))
